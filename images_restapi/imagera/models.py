383319d6da2bdcee79d949d49116211d329881ce
Madhukar S Holla <MadhukarS.Holla@citrix.com>
2018-05-16 15:02:07 +0530

    v0.2.0

--- images_restapi/imagera/models.py
+++ images_restapi/imagera/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class accessKey():
+    """ Access key for authentication """
+    key = models.CharField(unique = True, primary_key = True, max_length = 16)
+
+class imagera(models.Model):
+    """ Model for images """
+    key = models.CharField(max_length = 16)
+    image_id = models.CharField(primary_key = True, max_length = 32)
+    image_name = models.CharField(unique = True, max_length = 64)
+    image_path = models.CharField(max_length = 256)
+
+    def __str__(self):
+        """ Returns image name of the instance """
+        return self.image_name
+    
+    
