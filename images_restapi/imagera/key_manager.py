1ad4034e03f5a56f573dbedc2901cfcfda543386
Madhukar S Holla <mholla8@gmail.com>
2018-05-29 11:56:17 +0530

    imagera v0.6.0

--- images_restapi/imagera/key_manager.py
+++ images_restapi/imagera/key_manager.py
@@ -12,10 +12,11 @@ Functions to manage access keys and respective folders
 def gen_key():
     """Function to generate a new access key which does not exist already"""
     key = ''.join(choice(ascii_letters + digits) for _ in range(16))
+    folder = storage + key
 
-    while(validate_key(key)):
+    while(os.path.exists(folder)):
         key = ''.join(choice(ascii_letters + digits) for _ in range(16))
-
+        folder = storage + key
     return key
 
 
@@ -23,10 +24,8 @@ def validate_key(key):
     """Function to validate access key"""
     folder = storage + key
 
-    if(os.path.exists(folder)):
-        return True
-    else:
-        return False
+    if not os.path.exists(folder):
+        raise FileNotFoundError
 
 
 def generate_key_create_folder():
@@ -43,12 +42,8 @@ def regenerate_key(key):
     access key and rename the folder
     """
     key = key.strip()
-
-    if not validate_key(key):
-        return ("Error", key)
-    else:
-        folder = storage + key
-        newkey = gen_key()
-        newfolder = storage + newkey
-        os.rename(folder, newfolder)
-        return ("Success", newkey)
+    folder = storage + key
+    newkey = gen_key()
+    newfolder = storage + newkey
+    os.rename(folder, newfolder)
+    return newkey
